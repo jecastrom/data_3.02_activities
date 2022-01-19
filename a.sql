a9de6152ddd7b9517720a09689db7227d058c19c
jecastrom <jcastro.de@outlook.com>
2022-01-19 17:09:44 +0100

    a

--- a.sql
+++ a.sql
@@ -1,15 +1,15 @@
 SELECT
     d.A2 AS district_name,
-    c.client_id,
-    dp.account_id,
-    dp.`type` AS client_type
+    format(l.amount, 2, 'de_DE') AS amount_borrowed,
+    format(floor(avg(l.amount)), 2, 'de_DE') AS average_loan_amount
 FROM
-    client c
-    INNER JOIN district d ON c.district_id = d.A1
-    INNER JOIN disp dp ON c.client_id = dp.client_id
-WHERE
-    `type` = 'OWNER'
-ORDER BY
+    district d
+    INNER JOIN `client` c ON d.A1 = c.district_id
+    INNER JOIN `account` a ON c.district_id = a.district_id
+    INNER JOIN loan l ON a.account_id = l.account_id
+GROUP BY
     1
+ORDER BY
+    2 DESC
 LIMIT
     10;
