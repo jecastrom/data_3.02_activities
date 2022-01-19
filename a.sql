8116f04e6c06e9f3f8758085bd8ddd4dafbbe329
jecastrom <jcastro.de@outlook.com>
2022-01-19 18:33:33 +0100

    s

--- a.sql
+++ a.sql
@@ -1,6 +1,10 @@
 SELECT
-    *
+    client_id AS clients_with_no_credit_cards,
+    card_id
 FROM
-    district_overview
+    disp d
+    LEFT JOIN card c ON d.disp_id = c.disp_id
+WHERE
+    card_id IS NULL
 LIMIT
     10;
