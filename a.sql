4c85116d64b2bd49c76244b336a6fc214b730abb
jecastrom <jcastro.de@outlook.com>
2022-01-19 17:33:46 +0100

    a

--- a.sql
+++ a.sql
@@ -1,15 +1,6 @@
 SELECT
-    d.A2 AS district_name,
-    format(l.amount, 2, 'de_DE') AS amount_borrowed,
-    format(floor(avg(l.amount)), 2, 'de_DE') AS average_loan_amount
+    *
 FROM
-    district d
-    INNER JOIN `client` c ON d.A1 = c.district_id
-    INNER JOIN `account` a ON c.district_id = a.district_id
-    INNER JOIN loan l ON a.account_id = l.account_id
-GROUP BY
-    1
-ORDER BY
-    2 DESC
+    district_overview
 LIMIT
     10;
